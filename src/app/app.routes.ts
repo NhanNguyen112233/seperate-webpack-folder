d7a53cfa1ee823712992026011ad3b36fd9d4f24
nhannguyen1122 <nhannguyenn1122@gmail.com>
2024-10-01 00:04:32 +0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mfe1',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        exposedModule: './Module',
+      })
+        .then((m) => m.MfeComponentModule)
+        .catch((err) => console.error('Error loading remote module', err)),
+  },
+  // {
+  //   path: 'mfe2',
+  //   loadChildren: () =>
+  //     loadRemoteModule({
+  //       type: 'manifest',
+  //       // remoteEntry: 'http://localhost:4200/remoteEntry.js',
+  //       remoteName: 'mfe2',
+  //       exposedModule: './Module',
+  //     })
+  //       .then((m) => m.MfeComponentModule)
+  //       .catch((err) => console.error('Error loading remote module', err)),
+  // },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
