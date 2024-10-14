6d9d00b5d94827c81e2006e52bb16b284780d202
nhannguyenn112233 <nhannguyenn1122@gmail.com>
2024-10-15 00:32:13 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {AdminCommunicateService, AdminEvent} from 'admin-core-web-libs'
+import { Router, RouterOutlet } from '@angular/router';
+import { AdminCommunicateService, AdminEvent } from 'admin-core-web-libs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,22 +8,34 @@ import {AdminCommunicateService, AdminEvent} from 'admin-core-web-libs'
 })
 export class AppComponent implements OnInit {
   title = 'host-app';
-  loadContent=''
-  
-  constructor(@Inject(AdminCommunicateService) private adminCommunicateService: AdminCommunicateService) {}
+  loadContent = '';
 
-  ngOnInit(){
-    this.viewEventService()
+  constructor(
+    @Inject(AdminCommunicateService)
+    private adminCommunicateService: AdminCommunicateService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.viewEventService();
+
+    this.router.navigateByUrl(location.pathname.slice(1));
+    window.addEventListener('popstate', () => {
+      this.router.navigateByUrl(location.pathname.slice(1));
+    });
   }
-  loadEventService (){
-    this.adminCommunicateService.emmitEvent({event:'test',data:'test data'})
+  loadEventService() {
+    this.adminCommunicateService.emmitEvent({
+      event: 'test',
+      data: 'data gửi từ app cha',
+    });
   }
 
-  viewEventService (){
-     this.adminCommunicateService.getEvent().subscribe((data:AdminEvent)=>{
-      console.log('data',data);
-      
-      this.loadContent=data.data
-     })
+  viewEventService() {
+    this.adminCommunicateService.getEvent().subscribe((data: AdminEvent) => {
+      console.log('data', data);
+
+      this.loadContent = data.data;
+    });
   }
 }
