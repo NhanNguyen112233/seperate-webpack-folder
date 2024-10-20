d82804a3a8f7b207cc36b98bf175139b5a0d82ab
nhannguyenn112233 <nhannguyenn1122@gmail.com>
2024-10-21 02:05:47 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routes';
+import { ButtonComponent } from 'admin-core-web-libs';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
