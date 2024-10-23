3e678c5ad0e12b6addc4d7ee1f9a5940a3c45241
NhanNguyen112233 <71057395+NhanNguyen112233@users.noreply.github.com>
2024-10-23 08:34:45 +0000

    update

--- projects/admin-core-web-libs/src/lib/shared/components/date-picker/date-picker.component.ts
+++ projects/admin-core-web-libs/src/lib/shared/components/date-picker/date-picker.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { DateAdapter, MAT_DATE_FORMATS, MatNativeDateModule } from '@angular/material/core';
+import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker';
 import { CUSTOM_DATE_FORMATS, CustomDateAdapter } from './date-picker-adapter';
+import { CommonModule } from '@angular/common';
 
 
 // styling
@@ -11,17 +12,28 @@ import { CUSTOM_DATE_FORMATS, CustomDateAdapter } from './date-picker-adapter';
 @Component({
   selector: 'tpb-date-picker',
   standalone: true,
-  imports: [MatDatepickerModule],
+  imports: [MatDatepickerModule,CommonModule,MatNativeDateModule],
   providers:[{
     provide: MAT_DATE_FORMATS,
     useValue:CUSTOM_DATE_FORMATS
   },{
     provide: DateAdapter,
-    useFactory: ()=>CustomDateAdapter
+    useFactory: ()=>new CustomDateAdapter()
   }],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss'
 })
-export class DatePickerComponent {
+export class DatePickerComponent implements OnInit {
+ @Input() width:string="";
+ @Input() height:string="";
 
+ @ViewChild('picker',{static:true}) picker!:MatDatepicker<Date>;
+
+ constructor(private adapter:DateAdapter<Date>){
+ 
+ }
+
+ ngOnInit(){
+   this.adapter.setLocale('vi');
+ }
 }
