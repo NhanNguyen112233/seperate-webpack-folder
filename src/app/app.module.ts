e51ccd3cb87dea69bb6559979194d65740ef4ed8
nhannguyenn112233 <nhannguyenn1122@gmail.com>
2024-10-24 03:13:31 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routes';
-import { ButtonComponent, CheckboxComponent } from 'admin-core-web-libs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  AdminCoreWebLibModule,
+  ButtonComponent,
+  CheckboxComponent,
+  DatePickerComponent,
+  InputComponent,
+  SelectComponent,
+} from 'admin-core-web-libs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { enviroment } from '@config/enviroment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    AppRoutingModule,
+    AdminCoreWebLibModule.forRoot(enviroment),
     ButtonComponent,
+    SelectComponent,
+    DatePickerComponent,
     CheckboxComponent,
+
+    InputComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
