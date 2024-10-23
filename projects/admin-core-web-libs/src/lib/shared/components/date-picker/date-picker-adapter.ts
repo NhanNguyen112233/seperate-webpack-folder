b2bdb902d8ed0826cc5b54527d9ab2d8d4331b07
NhanNguyen112233 <71057395+NhanNguyen112233@users.noreply.github.com>
2024-10-23 10:18:43 +0000

    update

--- projects/admin-core-web-libs/src/lib/shared/components/date-picker/date-picker-adapter.ts
+++ projects/admin-core-web-libs/src/lib/shared/components/date-picker/date-picker-adapter.ts
@@ -1,107 +1,18 @@
 import { Injectable } from '@angular/core';
-import { DateAdapter, MAT_DATE_FORMATS, MatDateFormats } from '@angular/material/core';
+import { DateAdapter, MAT_DATE_FORMATS, MatDateFormats, NativeDateAdapter } from '@angular/material/core';
 
 @Injectable()
-export class CustomDateAdapter extends DateAdapter<Date> {
+export class CustomDateAdapter extends NativeDateAdapter {
   // Implement required methods for your custom date adapter
   // For example:
 
   constructor() {
     super();
   }
-  parse(value: any): Date | null {
-    // Custom parse logic
-    return new Date(value);
-  }
+ 
+  // format(date: Date, displayFormat: Object): string { 
 
-  format(date: Date, displayFormat: Object): string {
-    // Custom format logic
-    return date.toDateString();
-  }
-
-  getYear(date: Date): number {
-    return date.getFullYear();
-  }
-
-  getMonth(date: Date): number {
-    return date.getMonth();
-  }
-
-  getDate(date: Date): number {
-    return date.getDate();
-  }
-
-  getDayOfWeek(date: Date): number {
-    return date.getDay();
-  }
-
-  getMonthNames(style: 'long' | 'short' | 'narrow'): string[] {
-    // Custom logic for month names
-    return [];
-  }
-
-  getDateNames(): string[] {
-    // Custom logic for date names
-    return [];
-  }
-
-  getDayOfWeekNames(style: 'long' | 'short' | 'narrow'): string[] {
-    // Custom logic for day of week names
-    return [];
-  }
-
-  getYearName(date: Date): string {
-    return date.getFullYear().toString();
-  }
-
-  getFirstDayOfWeek(): number {
-    // Custom logic for first day of the week
-    return 0;
-  }
-
-  getNumDaysInMonth(date: Date): number {
-    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-  }
-
-  clone(date: Date): Date {
-    return new Date(date.getTime());
-  }
-
-  createDate(year: number, month: number, date: number): Date {
-    return new Date(year, month, date);
-  }
-
-  today(): Date {
-    return new Date();
-  }
-
-  addCalendarYears(date: Date, years: number): Date {
-    return new Date(date.setFullYear(date.getFullYear() + years));
-  }
-
-  addCalendarMonths(date: Date, months: number): Date {
-    return new Date(date.setMonth(date.getMonth() + months));
-  }
-
-  addCalendarDays(date: Date, days: number): Date {
-    return new Date(date.setDate(date.getDate() + days));
-  }
-
-  toIso8601(date: Date): string {
-    return date.toISOString();
-  }
-
-  isDateInstance(obj: any): boolean {
-    return obj instanceof Date;
-  }
-
-  isValid(date: Date): boolean {
-    return !isNaN(date.getTime());
-  }
-
-  invalid(): Date {
-    return new Date(NaN);
-  }
+  // }
 }
 
 export const CUSTOM_DATE_FORMATS: MatDateFormats = {
--- projects/admin-core-web-libs/src/lib/shared/components/date-picker/date-picker.component.ts
+++ projects/admin-core-web-libs/src/lib/shared/components/date-picker/date-picker.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { DateAdapter, MAT_DATE_FORMATS, MatNativeDateModule } from '@angular/material/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import {MatDatepicker, MatDatepickerModule} from '@angular/material/datepicker';
 import { CUSTOM_DATE_FORMATS, CustomDateAdapter } from './date-picker-adapter';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 // styling
@@ -12,18 +13,22 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'tpb-date-picker',
   standalone: true,
-  imports: [MatDatepickerModule,CommonModule,MatNativeDateModule],
+  imports: [MatDatepickerModule,CommonModule,FormsModule,MatNativeDateModule],
   providers:[{
     provide: MAT_DATE_FORMATS,
     useValue:CUSTOM_DATE_FORMATS
   },{
     provide: DateAdapter,
     useFactory: ()=>new CustomDateAdapter()
-  }],
+  },
+  { provide: MAT_DATE_LOCALE, useValue: 'en-US' }
+],
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss'
 })
 export class DatePickerComponent implements OnInit {
+  value=new Date();
+
  @Input() width:string="";
  @Input() height:string="";
 
