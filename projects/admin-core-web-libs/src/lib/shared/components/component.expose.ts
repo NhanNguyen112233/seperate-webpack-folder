863401ecfbc9eb726ad569f713913305719ee6ed
nhannguyenn112233 <nhannguyenn1122@gmail.com>
2024-11-02 10:34:41 +0700

    update

--- projects/admin-core-web-libs/src/lib/shared/components/component.expose.ts
+++ projects/admin-core-web-libs/src/lib/shared/components/component.expose.ts
@@ -4,3 +4,4 @@ export * from './input/input.component';
 export * from './checkbox/checkbox.component';
 export * from './date-picker/date-picker.component';
 export * from './modal/confirm-modal/confirm-modal.component';
+export * from './text-area/text-area.component';
